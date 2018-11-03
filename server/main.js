57fb2952642bdeca02eaa7c225f93ca4e599e6fe
Gibran Malheiros <gibatronic@gmail.com>
2018-11-03 19:10:18 +0100

    added env for server port

--- server/main.js
+++ server/main.js
@@ -6,7 +6,7 @@ const express = require('express');
 const photos = require('./photos');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 function listening() {
   process.stdout.write(`listening on port ${port}\n`);
